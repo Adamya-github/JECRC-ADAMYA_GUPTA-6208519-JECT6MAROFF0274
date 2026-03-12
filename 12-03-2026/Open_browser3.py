469103cb40b96ac33569ad4770974c011bd62db1
Adamya-github <121171569+Adamya-github@users.noreply.github.com>
2026-03-12 13:11:23 +0530

    Update Open_browser3.py

--- 12-03-2026/Open_browser3.py
+++ 12-03-2026/Open_browser3.py
@@ -8,7 +8,3 @@ driver.get("https://github.com/Adamya-github/JECRC-ADAMYA_GUPTA-6208519-JECT6MAR
 print(f'Adamya-github/JECRC-ADAMYA_GUPTA-6208519-JECT6MAROFF0274: {driver.current_url}')
 print(driver.name)
 sleep(5)
-
-
-## write a for loop for numerous browser , just open it
-## same for loop current url and title
